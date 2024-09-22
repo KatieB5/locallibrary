b875394e4122cefae5485935bd32d99d0b87352c
Katie Bickford <katie.bickford@thedatalab.org>
2024-09-22 17:47:15 +0100

    Created url, template, view and css file to render homepage

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
 # Create your views here.
+
+def home(request):
+
+    # Count the number of books and the number of instances of books
+
+    num_of_books = Book.objects.all().count()
+    num_book_instances = BookInstance.objects.all().count()
+
+    # Count number of books available for lending
+    num_available_instances = BookInstance.objects.filter(status__exact='a').count()
+
+    # Count the number of authors
+    num_of_authors = Author.objects.all().count()
+
+    context = {
+        'num_of_books': num_of_books,
+        'num_book_instances': num_book_instances,
+        'num_available_instances': num_available_instances,
+        'num_of_authors': num_of_authors,
+    }
+
+    return render(request, 'home.html', context=context)
