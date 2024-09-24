0305ca6fe28aee1999efaa617524802de3b8c88e
Katie Bickford <katie.bickford@thedatalab.org>
2024-09-24 15:27:58 +0100

    Created url mapping, views and templates for book list and individual book pages

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import Book, Author, BookInstance, Genre
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
@@ -16,11 +18,31 @@ def home(request):
     # Count the number of authors
     num_of_authors = Author.objects.all().count()
 
+    # Count number of genres
+    num_of_genres = Genre.objects.all().count()
+
+    # Count number of fiction books
+    num_fiction_books = Book.objects.filter(genre__name__iexact='Fiction').count()
+
+    # Count number of nonfiction books
+    num_nonfiction_books = Book.objects.filter(genre__name__iexact='Non-fiction').count()
+
+
     context = {
         'num_of_books': num_of_books,
         'num_book_instances': num_book_instances,
         'num_available_instances': num_available_instances,
         'num_of_authors': num_of_authors,
+        'num_of_genres': num_of_genres,
+        'num_fiction_books': num_fiction_books,
+        'num_nonfiction_books': num_nonfiction_books,
     }
 
     return render(request, 'home.html', context=context)
+
+class BookListView(generic.ListView):
+    model = Book
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
