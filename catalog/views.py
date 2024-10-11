3fa6d3f1c1f3b36f0259846f8840e13856581db6
Katie Bickford <katie.bickford@thedatalab.org>
2024-10-11 16:25:17 +0100

    Adds create book view for librarians.

--- catalog/views.py
+++ catalog/views.py
@@ -131,4 +131,9 @@ class AuthorDelete(PermissionRequiredMixin, DeleteView):
             return HttpResponseRedirect(
                 reverse("author-delete", kwargs={"pk": self.object.pk})
             )
+        
+class BookCreate(PermissionRequiredMixin, CreateView):
+    model = Book
+    fields = ['title', 'author', 'summary', 'isbn', 'genre']
+    permission_required = 'catalog.change_book'
     
